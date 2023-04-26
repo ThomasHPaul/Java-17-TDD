294e9bb59a96c17679da5fdf0bd08ccb914037da
Thomas Paul <thomaspauljs@outlook.com>
2023-04-26 06:46:18 -0500

    Continuing TDD in Java SE 17

--- src/main/java/com/tdd/m3/StringUtil.java
+++ src/main/java/com/tdd/m3/StringUtil.java
@@ -5,10 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class StringUtil {
 
-    public static String truncate(String input, int limit) {
-        if(input.length() <= limit) {
+    //TODO: test around the new cutOffChars parameter
+    public static String truncate(String input, int limit, String cutOffChars) {
+        if(input == null) {
+            throw new IllegalArgumentException("String input must not be null");
+        }
+        if(limit < 1) {
+            throw new IllegalArgumentException("Limit input must be greater than 0");
+        }
+
+        String ellipsis = "...";
+        if(inputTooShort(input, limit, ellipsis)) {
             return input;
         }
-        return input.substring(0, limit) + "...";
+
+        return input.substring(0, limit) + ellipsis;
     }
+
+    public static String truncateWithEllipsis(String input, int limit) {
+        return truncate(input, limit, "...");
+    }
+
+    private static boolean inputTooShort(String input, int limit, String ellipsis) {
+        return input.length() <= ellipsis.length() || input.length() <= limit;
+    }
+
+
 }
--- src/test/java/com/tdd/m3/StringUtilTest.java
+++ src/test/java/com/tdd/m3/StringUtilTest.java
@@ -2,33 +2,65 @@ package com.tdd.m3;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class StringUtilTest {
 
-    @Test
-    public void limitReached_StringTruncates() {
-
-        String input = "The economy is about to";  // length 23
-        int limit = 11;
-
-        Assertions.assertEquals("The economy...", StringUtil.truncate(input, limit));
+    @ParameterizedTest
+    @MethodSource("validLimitProvider")
+    public void limitReached_stringTruncates(int limit, String output) {
+        String input = "The economy is about to";
+        Assertions.assertEquals(output, StringUtil.truncate(input, limit));
     }
 
-    @Test
-    public void limitNotReached_StringNotChanged() {
+    public static Stream<Arguments> validLimitProvider() {
+        return Stream.of(
+                Arguments.of(1, "T..."),
+                Arguments.of(11, "The economy...")
+        );
+    }
 
-        String input = "The economy is about to";  // length 23
-        int limit = 50;
-
+    @ParameterizedTest
+    @MethodSource("inputOutputLimitProvider")
+    public void limitNotReached_StringNotChanged(String input, int limit) {
         Assertions.assertEquals("The economy is about to", StringUtil.truncate(input, limit));
     }
 
-    @Test
-    public void limitNotReachedAtBorder_StringNotChanged() {
+    public static Stream<Arguments> inputOutputLimitProvider() {
+        String input = "The economy is about to";
+        return Stream.of(
+        Arguments.of(input, 40),
+        Arguments.of(input, input.length()) // at border, input length == limit
+        );
+    }
 
-        String input = "The economy is about to";  // length 23
-        int limit = 23;
+    @ParameterizedTest
+    @MethodSource("invalidArgumentProvider")
+    public void invalidInput_isRejected() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> StringUtil.truncate(null, 5));
+    }
 
-        Assertions.assertEquals("The economy is about to", StringUtil.truncate(input, limit));
+    public static Stream<Arguments> invalidArgumentProvider() {
+        return Stream.of(
+                Arguments.of(null, 5),
+                Arguments.of("Some input", 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("shortInputLessOrEqualToEllipsis")
+    public void inputShorterOrEqualThanLimit_StringNotChanged(String input, int limit) {
+        Assertions.assertEquals(input, StringUtil.truncate(input, limit));
+    }
+
+    public static Stream<Arguments> shortInputLessOrEqualToEllipsis() {
+        return Stream.of(
+                Arguments.of("The", 2),
+                Arguments.of("The", 3)
+        );
     }
 }
